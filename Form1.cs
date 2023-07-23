6c40fa5f011241a6133bd06948f87c282a2d6562
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2023-07-23 10:10:38 +0800

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stmple_webbrowse_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://bilibili.com");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://minecraft.net");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://microsoft.com");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://sungbly1tsstt.github.io");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://stsvideo.github.io");
+        }
+    }
+}
